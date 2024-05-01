cc12916a9725563c971f4a945b26c767a73316bf
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-01 15:02:50 +0700

    update string

--- basic-go/string.go
+++ basic-go/string.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
-func main(){
-  fmt.Println("Rahman")
-  fmt.Println("Rahman RVBR")
-  fmt.Println("Rahman RVBR Gaming")
-}
+// func main(){
+//   fmt.Println("Rahman")
+//   fmt.Println("Rahman RVBR")
+//   fmt.Println("Rahman RVBR Gaming")
+// }
 
 // function untuk string
 // len("string") = menghitung jumlah karakter di string
 // "string"[number] = mengambil karakter pada posisi yang ditentukan
+
+func main(){
+  fmt.Println(len("Rahman"))
+  fmt.Println("Rahman RVBR"[0]) /* =>  hasil yang di dapat dalam bentuk byte */
+  fmt.Println("Rahman RVBR Gaming"[1]) /* =>  hasil yang di dapat dalam bentuk byte */
+}
