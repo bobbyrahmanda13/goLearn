c9ad84da5651d1038183cb6c45ec866e573fe312
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-01 14:38:06 +0700

    update string

--- basic-go/string.go
+++ basic-go/string.go
@@ -7,3 +7,7 @@ func main(){
   fmt.Println("Rahman RVBR")
   fmt.Println("Rahman RVBR Gaming")
 }
+
+// function untuk string
+// len("string") = menghitung jumlah karakter di string
+// "string"[number] = mengambil karakter pada posisi yang ditentukan
