78fe4902eb77ff5f3eb3a60695b4836a13bb0ebc
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-01 15:27:44 +0700

    update variable

--- basic-go/variable.go
+++ basic-go/variable.go
@@ -2,12 +2,44 @@ package main
 
 import "fmt"
 
-func main(){
-  var name string
+// step 1
+// func main(){
+//   var name string // jika type data pada name dihilangkan maka akan error
+//
+//   name = "Rahman Gaming"
+//   fmt.Println(name)
+//
+//   name = "Rahman Ganteng"
+//   fmt.Println(name)
+// }
 
-  name = "Rahman Gaming"
-  fmt.Println(name)
+// step 2 = cara lansung karena golang tau kita mau menggunakan tipe data apa akan di detect secara otomatis
+// func main() {
 
-  name = "Rahman Ganteng"
-  fmt.Println(name)
-}
+	// var name string = "Rahman Gaming" // cara ini menjadi tidak wajib karena golang sudah tau type data yang digunakan jadi percuma bikin kek ginian
+// 	var name = "Rahman Gaming"
+// 	fmt.Println(name)
+//
+// 	name = "Rahman Ganteng"
+// 	fmt.Println(name)
+// }
+
+// step 3 => pada golang kata kunci var tidak harus dibuat / tidak lah wajib, jadi bisa digantikan dengan ":=" saat menginisialisasikan data pada variable tersebut
+
+// func main(){
+  // name := "Rahman Visalux" => ini cuman deklarsikan datanya saja jadi jika sudah dibuat maka tidak perlu membuat nya lagi jadi dibuat hanya untuk deklarasikan data jadi itu dibuat di awal saja
+  // name := "Rahman Visalux"
+  // fmt.Println(name)
+
+  // name = "Rahman Gaming" => jadi tidak diperbolahkan untuk dibuat lagi, jika misal dibuat lagi maka itu akan seperti variabel tersebut dibuat ulang / di deklarasikan ulang type  / variabel datanya
+//   name = "Rahman Gaming"
+//   fmt.Println(name)
+// }
+
+// Deklarasi Multiple Variable
+// di golang kita bisa membuat banyak variabel sekaligus
+// code yang dibuat akan lebih bagus dan mudah dibaca
+
+
+
+
