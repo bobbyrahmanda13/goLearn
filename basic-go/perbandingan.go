deba2fda62fa138a9e57509dce2b7a3c21252d6b
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-11 14:26:26 +0700

    added perbandingan

---
+++ basic-go/perbandingan.go
@@ -0,0 +1,15 @@
+// # Operasi perbandingan
+// - operasi perbandingan adalah operasi untuk membandingkan dua buah data
+// - operasi perbandingan adalah operasi yg menghasilkan nilai boolean ( benar / salah )
+// - jika hasl operasinya adalah benar, maka nilainya adalah true
+// - jika hasl operasinya adalah salah, maka nilainya adalah false
+
+// # Tabel Operator Perbandingan
+//
+// Operator | Keterangan
+// > | Lebih dari
+// < | Kurang dari
+// >= | Lebih dari Sama Dengan
+// <= | Kurang dari Sama Dengan>
+// == | Sama Dengan
+// != | Tidak Sama Dengan
