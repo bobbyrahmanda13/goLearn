751d0ff3bc24350ba4f52faedb67f329984d1b6e
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-12 16:41:59 +0700

    update operasi boolean

--- basic-go/operasiBoolean.go
+++ basic-go/operasiBoolean.go
@@ -1,15 +1,56 @@
 // Operasi Boolean
 // Operator | Keterangan
 // - && | Dan
-// - || | Atau 
+// - || | Atau
 // - !  | Kebalikan
 
-// Operasi &&
+// Operasi && = dan / AND
+
 // Nilai 1 | Operator | Nilai 2 | Hasil
+// -------------------------------------
 // true    | &&       | true    | true
 // true    | &&       | false   | false
 // false   | &&       | true    | false
 // false   | &&       | false   | false
+// -------------------------------------
+// Note !! => jika salah satunya itu false maka hasil nya adalah false
 
+// Operasi || = atau / OR
 
+// Nilai 1 | Operator | Nilai 2 | Hasil
+// -------------------------------------
+// true    | ||       | true    | true
+// true    | ||       | false   | true
+// false   | ||       | true    | true
+// false   | ||       | false   | false
+// -------------------------------------
+// Note !! => jika salah satu nya itu true maka hasil nya adalah true
 
+// Operasi ! = kebalikan
+
+// Operator | Nilai 2 | Hasil
+// -------------------------------------
+// !       | true    | true
+// !       | false   | true
+// -------------------------------------
+
+// Kode Program Operasi Boolean
+
+package main
+
+import "fmt"
+
+func main(){
+  var nilaiAkhir = 90
+  var absensi = 80
+
+  var lulusNilaiAkhir bool = nilaiAkhir > 80 // true
+  var lulusAbsensi bool = absensi > 80 // false
+
+  var lulus bool = lulusNilaiAkhir && lulusAbsensi // false
+
+  var lulus2 bool = lulusNilaiAkhir || lulusAbsensi // true
+
+  fmt.Println(lulus) // false
+  fmt.Println(lulus2) // true
+}
