6ca9fe4b4237fdf99c415bbe15a008271ebd7b40
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-12 15:54:22 +0700

    add or update operasi boolean

--- basic-go/operasiBoolean.go
+++ basic-go/operasiBoolean.go
@@ -4,4 +4,12 @@
 // - || | Atau 
 // - !  | Kebalikan
 
+// Operasi &&
+// Nilai 1 | Operator | Nilai 2 | Hasil
+// true    | &&       | true    | true
+// true    | &&       | false   | false
+// false   | &&       | true    | false
+// false   | &&       | false   | false
+
+
 
