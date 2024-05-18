8d5747ae6299f7615470e466e6b61a81a5a89fe5
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-18 17:12:39 +0700

    update array.go

--- basic-go/array.go
+++ basic-go/array.go
@@ -41,13 +41,25 @@ func main() {
 	fmt.Println(values[2])
 	fmt.Println(values)
 
-  // Function Array
-  // Operasi | Keterangan
-  // len(array) | Untuk mendapatkan panjang Array 
-  // array[index] | Mendapatkan data di posisi index
-  // array[index] = value | mengubah data di posisi index
+	// Function Array
+	// Operasi | Keterangan
+	// len(array) | Untuk mendapatkan panjang Array
+	// array[index] | Mendapatkan data di posisi index
+	// array[index] = value | mengubah data di posisi index
 
+	// contoh kode Program array
+	var valuess = [...]int{ // [...] tanda ini adalah untuk menampung berapa banyak array yang ingin dimasukkan, jadi jika kita tidak tau ingin memasukkan jumlah data array nya berapa maka buat saja tanda itu
+		//! note menggunakan tanda [...] datanya harus di deklarasikan dahulu jika tidak akan error
+		90,
+		80,
+		95,
+		100,
+		120,
+	}
 
-
+	fmt.Println(valuess)
+	fmt.Println("total banyak data dalam array :", len(valuess))
+	valuess[0] = 100 // format valuess[0] = 100 => valuess adalah array, [0] ini = index, 100 adalah value
+	fmt.Println(valuess)
 
 }
