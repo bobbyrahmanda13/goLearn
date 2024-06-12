ac230f26d46a921ac3bd2b8db535bba85809d02b
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-12 20:26:45 +0700

    add if-expression dalam golang

---
+++ basic-go/ifexpression.go
@@ -0,0 +1,4 @@
+// if Expression
+// if adalah salah satu kata kunci yang digunakan untuk percabangan
+// Percabangan artinya kita bisa mengeksekusi kode program tertentu ketika suatu kondisi terpenuhi
+// Hampir di semua bahasa pemrograman mendukung if Expression
