6cd73c4da32a12bd45fa7d75900619ce9b648d50
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-16 14:09:37 +0700

    update if expression golang

--- basic-go/ifexpression.go
+++ basic-go/ifexpression.go
@@ -2,3 +2,27 @@
 // if adalah salah satu kata kunci yang digunakan untuk percabangan
 // Percabangan artinya kita bisa mengeksekusi kode program tertentu ketika suatu kondisi terpenuhi
 // Hampir di semua bahasa pemrograman mendukung if Expression
+
+
+package main
+
+import "fmt"
+
+func main() {
+
+  // var name string = "eko"
+  name := "Eko" // => type data string
+  jancok := false // => type data boolean
+  // jancok1 := 223 // => type data number
+
+  if name == "Eko" {
+		fmt.Println("Hello World !")
+	}
+  if jancok == false {
+    fmt.Println("truee false ")
+  }
+
+}
+
+// Else Expression
+
