2f82ba953eeaefb5a53f24386b0313f7ad66e429
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-19 20:08:15 +0700

    clear if expression go

--- basic-go/ifexpression.go
+++ basic-go/ifexpression.go
@@ -31,17 +31,55 @@ func main() {
 	// kadang kita ingin melakukan eksekusi program tertentu jika kondisi if bernilai false
 	// hal ini bisa dilakukan menggunakan else expression
 
-  // Else If Expression
-  // - Kadang dalam if, kita butuh membuat beberapa kondisi
-  // - Kasus seperti ini, kita bisa menggunakan else if expression
-  // - jika kondisi if dan else if tidak terpenuhi maka dapat menambahkan else
+	// Else If Expression
+	// - Kadang dalam if, kita butuh membuat beberapa kondisi
+	// - Kasus seperti ini, kita bisa menggunakan else if expression
+	// - jika kondisi if dan else if tidak terpenuhi maka dapat menambahkan else
 
-  // # Kode program if Else
+	// # Kode program if Else
 
-  nama :="Rahman"
+	nama := "Riko"
 
-  if name == "Rahman" {
-    fmt.Println("Hello", name)
+	if nama == "Rahman" {
+		fmt.Println("Hello", nama)
+	} else if nama == "Riko" {
+		fmt.Println("Hello", nama)
+	} else {
+		fmt.Println("Hai Boleh Kenalan")
+	}
+
+  // IF dengan short statement
+  // if mendukung short statement sebelum kondisi 
+  // hal ini sangat cocok untuk membuat statement yang sederhana sebelum melakukan pengecekan terhadap kondisi
+
+  // Kode Program if short statement
+
+  // biasanya seperti ini :
+
+  // taik := "visalux" 
+  // masha := len(taik)
+  //
+  // if masha > 5 {
+  //   fmt.Println("Nama Terlalu panjang")
+  // } else {
+  //   fmt.Println("Nama sudah benar")
+  // }
+
+  // Kode Program if short statement
+
+  taik := "visalux" 
+
+  if masha := len(taik); masha > 5 {
+    fmt.Println("Nama Terlalu panjang")
+  } else {
+    fmt.Println("Nama sudah benar")
   }
 
+  // cat :
+  // masha adalah variabel, len(taik) = statement len menghitung jumlah karakter, taik = variabel yang berisi string  kata "visalux", ; (titik koma) = sebagai pembatas untuk kondisi
+  // masha > 5 => maksudnya variabel masha yang berisi panjang karakter dari variabel taik, lalu > 5 yang jika angka nya lebih dari 5 maka tercetak "Nama Terlalu Panjang", jika tidak terpenuhi (else) maka yang tercetak adalah "Nama sudah benar"
+
+
+
 }
+
