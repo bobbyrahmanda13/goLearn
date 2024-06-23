bfaaf5774e2ac5c1ea6c321d19b989c5e6938c0e
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-23 17:38:16 +0700

    update add switch expression go

---
+++ basic-go/switch-expression.go
@@ -0,0 +1,29 @@
+// Switch Expression
+// - Selain if expression, untuk melakukan percabangan, kita juga bisa menggunakan Switch Expression
+// - Switch expression sangat sederhana dibandingan if
+// - Biasanya switch expression digunakan untuk melakukan pengecekan ke kondisi dalam satu variable
+
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Rahman"
+
+	switch name {
+
+	case "Rahman":
+		// fmt.Println("Hello ", name)
+		fmt.Println("Hello Rahman")
+
+	case "Richo":
+		// fmt.Println("Hello ", name)
+		fmt.Println("Hello Richo")
+
+	default:
+		fmt.Println("Hi, Boleh Kenalan ?")
+
+	}
+}
+
