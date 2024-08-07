49499fae4cbfb4aa910aeeab313b7b092dd1f83d
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-08-07 19:47:45 +0700

    update typo

--- basic-go/slice.go
+++ basic-go/slice.go
@@ -97,7 +97,7 @@ func main() {
 	// len(slice) | Untuk mendapakan panjang
 	// cap(slice) | Untuk mendapatkan kapasitas
 	// append(slice,data) | Membuat slice baru dengan menambah data ke posisi terakhir slice, jika kapasitas sudah penuh, maka akan membuat array baru
-	// make([]TypeData, length, capcity) | Membuat slice baru
+	// make([]TypeData, length, capacity) | Membuat slice baru
 	// copy(destination, source) | menyalin slice dari source ke destination
 
 	// # Kode Program Append Slice
