c0ae607ae5564b95dee0d7551a76b95a17fe9135
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-08-29 21:09:04 +0700

    add function.go

---
+++ basic-go/function.go
@@ -0,0 +1,8 @@
+// # Function
+
+// - sebelumnya kita sudah mengenal sebuah function yang wajib dibuat agar program kita bisa berjalan, yaitu function main()
+// - Function adalah sebuah blok kode yang sengaja dibuat dalam program agar bisa digunakan berulang-ulang
+// - Cara membuat function sangat sederhana, hanya dengan menggunakan kata kunci "func" lalu diikuti dengan nama function nya dan blok kode isi function nya
+// - Setelah membuat function, kita bisa mengeksekusi function tersebut dengan memanggilnya menggunakan kata kunci nama function nya diikuti tanda kurung buka "{", dan kurung tutup "}"
+
+
