1cdc40fc0797551258ba955a25f8a7683d833016
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-10-24 05:05:46 +0700

    update uploadfile

--- basic-go/go.mod
+++ basic-go/go.mod
@@ -1,9 +1,3 @@
 module basic-go
 
-go 1.22.2
-
-require (
-	github.com/PuerkitoBio/goquery v1.9.2 // indirect
-	github.com/andybalholm/cascadia v1.3.2 // indirect
-	golang.org/x/net v0.24.0 // indirect
-)
+go 1.23.2
---
+++ uploadFile/main.go
@@ -0,0 +1,48 @@
+// ⚡️ Fiber is an Express inspired web framework written in Go with ☕️
+// 🤖 Github Repository: https://github.com/gofiber/fiber
+// 📌 API Documentation: https://docs.gofiber.io
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Fiber instance
+	app := fiber.New()
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		// Parse the multipart form:
+		form, err := c.MultipartForm()
+		if err != nil {
+			return err
+		}
+		// => *multipart.Form
+
+		// Get all files from "documents" key:
+		files := form.File["Documents"]
+		// => []*multipart.FileHeader
+
+		// Loop through files:
+		for _, file := range files {
+			fmt.Println(file.Filename, file.Size, file.Header["Content-Type"][0])
+			// => "tutorial.pdf" 360641 "application/pdf"
+
+			// Save the files to disk:
+			err := c.SaveFile(file, fmt.Sprintf("./%s", file.Filename))
+
+			// Check for errors
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
