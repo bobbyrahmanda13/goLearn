3faa8093e47e2536fb27c362fd466d3663d8ae19
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-10-27 20:14:39 +0700

    update functionReturnValue

--- basic-go/functionReturnValue.go
+++ basic-go/functionReturnValue.go
@@ -9,12 +9,23 @@ package main
 import "fmt"
 
 func getHai(nama string) string {
-  return "Hai " + nama
+
+  // return "Hai " + nama
+
+  hello:="Hello " + nama
+
+  return hello
 }
 
 func main(){
   // buat variabel result
   result := getHai("Rahman")
+
   fmt.Println(result)
+  fmt.Println(getHai("Budi"))
+  fmt.Println(getHai("joko"))
+  fmt.Println(getHai("jancok"))
+
 }
+
 
