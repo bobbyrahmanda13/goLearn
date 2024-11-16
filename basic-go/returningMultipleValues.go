288aabadb4f6bd179122f02ea9353f2655a33b17
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-11-16 21:14:32 +0700

    update returning multiple values kode program

--- basic-go/returningMultipleValues.go
+++ basic-go/returningMultipleValues.go
@@ -3,4 +3,15 @@
 // - Function tidak hanya dapat mengembalikan satu value, tapi juga bisa multiple value
 // - Untuk memberitahu jika function mengembalikan multiple value, kita harus menulis semua tipe data return value nya di function
 
+package main
 
+import "fmt"
+
+func getFullName() (string, string) {
+	return "Rahman", "Visalux"
+}
+
+func main() {
+	firstName, lastName := getFullName()
+	fmt.Println(firstName, lastName)
+}
