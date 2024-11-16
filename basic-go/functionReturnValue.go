a2e10b94ee7be48a23f72b6bc1e3abc278112415
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-11-16 21:05:42 +0700

    remove indent and update comment

--- basic-go/functionReturnValue.go
+++ basic-go/functionReturnValue.go
@@ -1,7 +1,7 @@
 // # Function Return Value
 // - function bisa mengembalikan data
 // - Untuk memberitahu bahwa function mengambalikan data, kita harus menuliskan tipe data kembalian dari function tersebut
-// - Jika function tersebut kita deklarasikan dengan tipe data pengembalian, maka wajib di dalam function nya kita harus mengembalikan data
+// - Jika function tersebut kita deklarasikan dengan tipe data pengembalian, maka wajib di dalam function nya kita harus mengembalikan data (menggunakan "return")
 // - Untuk mengambalikan data dari function, kita bisa menggunakan kata kunci return, diikuti dengan datanya
 
 package main
@@ -20,8 +20,8 @@ func getHai(nama string) string {
 func main(){
   // buat variabel result
   result := getHai("Rahman")
-
   fmt.Println(result)
+
   fmt.Println(getHai("Budi"))
   fmt.Println(getHai("joko"))
   fmt.Println(getHai("jancok"))
