5c52b301aadaef840126b5194ade28e53a81b83a
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-11-16 21:08:50 +0700

    add returning multiple values comment

---
+++ basic-go/returningMultipleValues.go
@@ -0,0 +1,6 @@
+//# Returning Multiple Values
+
+// - Function tidak hanya dapat mengembalikan satu value, tapi juga bisa multiple value
+// - Untuk memberitahu jika function mengembalikan multiple value, kita harus menulis semua tipe data return value nya di function
+
+
