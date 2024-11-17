81ae17ccd2744ddfc9ebd2dff96feab5e4d93b9b
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-11-17 17:47:56 +0700

    add multiple return values

--- basic-go/returningMultipleValues.go
+++ basic-go/returningMultipleValues.go
@@ -11,9 +11,23 @@ func getFullName() (string, string) {
 	return "Rahman", "Visalux"
 }
 
+func getGameName() (string, string) {
+	return "GOD", "Visalux"
+}
+
 func main() {
 
 	firstName, lastName := getFullName()
-	fmt.Println(firstName, lastName)
+	fmt.Println(firstName, lastName) // butuh semuanya
+
+  // # Menghiraukan Return Value
+  // - Multiple return value wajib ditangkap value nya
+  // - Jika kita menghiraukan return value tersebut, kita bisa menggunakan tanda _ (garis bawah)
+
+	_, namaAkhir := getGameName()
+  fmt.Println(namaAkhir)
+
+	namaAwal, _ := getGameName()
+  fmt.Println(namaAwal)
 }
 
