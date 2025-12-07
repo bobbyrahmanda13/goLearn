019fc33104e62c15bdb7d0fbd485ce675535196d
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-07 16:06:00 +0700

    update constant

--- basic-go/7constant.go
+++ basic-go/7constant.go
@@ -1,22 +1,27 @@
 // constant
 package main
 
-// - adalah variable yang nilai nya tidak bisa diubah lagi setelah pertama kali diberi nilai
-// - cara pembuatannya constant mirip dengan variable, yg membedakannya hanya kata kunci yang digunakan adalah const, bukan var
-// - saat pembuatan constant, kita wajib lansung menginisialisasikan datanya package main
+/*
+- adalah variable yang nilai nya tidak bisa diubah lagi setelah pertama kali diberi nilai
+- cara pembuatannya constant mirip dengan variable, yg membedakannya hanya kata kunci yang digunakan adalah const, bukan var
+- saat pembuatan constant, kita wajib lansung menginisialisasikan datanya
+*/
 func main() {
-	// cara biasa
-	// const firstName string = "Rahman"
-	// const lastName = "Gaming"
+	/*
+		cara biasa
+		const firstname string = "rahman"
+		const lastname = "gaming"
+	*/
 
 	// Deklarasi ultiple constant => membuat constant dengan sekaligus banyak
+
 	const (
 		firstName string = "Rahman"
 		lastName         = "juancok"
 	)
-
-	// error
-	// firstName = "Tidak bisa diubah"
-	// lastName = "Tidak bisa diubah"
-
+	/*
+	   error
+	   firstName = "Tidak bisa diubah"
+	   lastName = "Tidak bisa diubah"
+	*/
 }
