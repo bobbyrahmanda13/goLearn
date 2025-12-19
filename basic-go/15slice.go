7d3b671dbc896e20e5c2d88ecd6353fc1367baba
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-20 00:04:48 +0700

    update

--- basic-go/15slice.go
+++ basic-go/15slice.go
@@ -15,49 +15,53 @@ kalau data array sudah di tentukan misalnya data array nya adalah 5 ya, itu jadi
 - capacity adalah kapasitas dari slice, dimana length tidak boleh lebih dari capacity
 */
 
-// * Membuat Slice dari Array
+/*
+* Membuat Slice dari Array
 
-// Membuat Slice | Keterangan
-// array[low:hight] | Membuat Slice dari Array dimulai index low sampai index sebelum high
-// array[low:] | Membuat Slice dari Array dimulai dari index low sampai index akhir di Array
-// array[:high] | Membuat Slice dari Array dimulai dari index 0 sampai index sebelum high
-// array[:] | Membuat Slice dari Array dimulai dari index 0 sampai index akhir di Array
+Membuat Slice | Keterangan
+array[low:hight] | Membuat Slice dari Array dimulai index low sampai index sebelum high
+array[low:] | Membuat Slice dari Array dimulai dari index low sampai index akhir di Array
+array[:high] | Membuat Slice dari Array dimulai dari index 0 sampai index sebelum high
+array[:] | Membuat Slice dari Array dimulai dari index 0 sampai index akhir di Array
+*/
 
-// Slice dan Array
-//
-// contoh :
-//
-// array{
-//   0 = "January",
-//   1 = "February",
-//   2 = "Maret",
-//   3 = "April",
-//   4 = "Mei",
-//   5 = "Juni",
-//   6 = "Juli",
-//   7 = "Agustus",
-//   8 = "September",
-//   9 = "Oktober",
-//   10 = "November",
-//   11 = "Desember",
-// }
-//
-// array[4:7]
-//
-// - slice 1 :
-// -- pointer = 4 => index ke 4 => penunjuk data pertamanya
-// -- length = 3 => panjang data dimulai dari data pertama, karena sebelum high maka dari data 4 5 6 = 3 total length nya jadi dari 4 - 7 itu jarrak nya 3
-// -- capacity = 8 => total semua kapasitas dari array nya dimulai dari index ke 4 data pertama(pointer)
-//
-// contoh lainnya
-//
-// array[6:9]
-//
-// - slice 2 :
-// -- pointer = 6
-// -- length = 3 => karena dari 6 ke 9 itu jaraknya 3
-// -- capacity = 6 => karena dari data pertama yaitu index ke 6 hingga ke index data terakhir, karena array nya berisi sampai index ke 11 maka dari 6 ke 11 itu adalah 6 7 8 9 10 11
-//
+/*
+Slice dan Array
+
+contoh :
+
+array{
+  0 = "January",
+  1 = "February",
+  2 = "Maret",
+  3 = "April",
+  4 = "Mei",
+  5 = "Juni",
+  6 = "Juli",
+  7 = "Agustus",
+  8 = "September",
+  9 = "Oktober",
+  10 = "November",
+  11 = "Desember",
+}
+
+array[4:7]
+
+- slice 1 :
+-- pointer = 4 => index ke 4 => penunjuk data pertamanya
+-- length = 3 => panjang data dimulai dari data pertama, karena sebelum high maka dari data 4 5 6 = 3 total length nya jadi dari 4 - 7 itu jarrak nya 3
+-- capacity = 8 => total semua kapasitas dari array nya dimulai dari index ke 4 data pertama(pointer)
+
+contoh lainnya
+
+array[6:9]
+
+- slice 2 :
+-- pointer = 6
+-- length = 3 => karena dari 6 ke 9 itu jaraknya 3
+-- capacity = 6 => karena dari data pertama yaitu index ke 6 hingga ke index data terakhir, karena array nya berisi sampai index ke 11 maka dari 6 ke 11 itu adalah 6 7 8 9 10 11
+
+*/
 
 // # Kode Program Slice
 
@@ -79,28 +83,32 @@ func main() {
 	var slice3 []string = names[3:]
 	fmt.Println(slice3)
 
-	//! note: jika sudah punya array lalu jika ingin membuat slice dari array yg sudah ada
-	// maka bentuk slice nya akan seperti ini:
+	//  NOTE: jika sudah punya array lalu jika ingin membuat slice dari array yg sudah ada
+	//  NOTE: maka bentuk slice nya akan seperti ini:
 
-	// slice4 := names[:]
-	// fmt.Println(slice4)
+	/*
+		slice4 := names[:]
+		fmt.Println(slice4)
 
-	// jika menggunakan deklarasi manual menggunakan var
-	// cara manual
-	var slice4 []string = names[:]
-	fmt.Println("ini slice4", slice4)
+		jika menggunakan deklarasi manual menggunakan var
+		cara manual
+		var slice4 []string = names[:]
+		fmt.Println("ini slice4", slice4)
+	*/
 
-	//! ket : jika menggunakan cara manual maka untuk slice itu tidak perlu menentukan jumlah data
-	// seperti jika di array => [3]string, jika di slice => []string, jadi tidak dibutuhkan untuk menentukan jumlah data array nya
+	//  NOTE: jika menggunakan cara manual maka untuk slice itu tidak perlu menentukan jumlah data
+	// NOTE: seperti jika di array => [3]string, jika di slice => []string, jadi tidak dibutuhkan untuk menentukan jumlah data array nya
 
 	// # Function Slice
 
-	// Operasi | Keterangan
-	// len(slice) | Untuk mendapakan panjang
-	// cap(slice) | Untuk mendapatkan kapasitas
-	// append(slice,data) | Membuat slice baru dengan menambah data ke posisi terakhir slice, jika kapasitas sudah penuh, maka akan membuat array baru
-	// make([]TypeData, length, capacity) | Membuat slice baru
-	// copy(destination, source) | menyalin slice dari source ke destination
+	/*
+		Operasi | Keterangan
+		len(slice) | Untuk mendapakan panjang
+		cap(slice) | Untuk mendapatkan kapasitas
+		append(slice,data) | Membuat slice baru dengan menambah data ke posisi terakhir slice, jika kapasitas sudah penuh, maka akan membuat array baru
+		make([]TypeData, length, capacity) | Membuat slice baru
+		copy(destination, source) | menyalin slice dari source ke destination
+	*/
 
 	// # Kode Program Append Slice
 	fmt.Println("---------------------batas---------------------")
