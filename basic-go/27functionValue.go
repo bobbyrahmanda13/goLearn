535c8cb4b8ff864a771f63dff4c1d4c036fc5581
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-29 21:55:06 +0700

    update function value

--- basic-go/27functionValue.go
+++ basic-go/27functionValue.go
@@ -14,7 +14,19 @@ func getGoodBye(name string) string {
 	return "Good Bye " + name
 
 }
+
+func sayangSaranghaee(name string) string {
+	return "Sayang " + name
+}
+
 func main() {
 	goodbye := getGoodBye
 	fmt.Println(goodbye("Rahman"))
+
+	contoh := sayangSaranghaee
+	misal := sayangSaranghaee
+
+	fmt.Println(contoh("Rahman"))
+	fmt.Println(misal("Rahman"))
+
 }
