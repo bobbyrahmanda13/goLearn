380b619523ddc41a11095fb38c63919d90faa031
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-01-01 12:43:42 +0700

    update

---
+++ basic-go/28functionParameter.go
@@ -0,0 +1,13 @@
+/*
+# Function as Parameter (sebagai parameter)
+- function tidak hanya bisa kita simpan di dalam variable sebagai value
+- namun juga bisa kita gunakan sebagai parameter untuk function lain
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
