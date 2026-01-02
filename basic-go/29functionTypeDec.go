f51762da59c2c9740cc37569d88103597c2633e9
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-01-02 23:25:20 +0700

    update

---
+++ basic-go/29functionTypeDec.go
@@ -0,0 +1,4 @@
+/* Function Type Declaration
+- Kadang jika function terlalu panjang,  agak ribet untuk menuliskannya di dalam parameter
+- Type Declarations juga bisa digunakan membuat alias function, sehingga akan mempermudah kita menggunakan function sebagai parameter
+*/
