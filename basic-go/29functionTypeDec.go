4028d7e95cef750e79fc1bd004a8afffd3a0d42e
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-01-03 17:58:05 +0700

    update

--- basic-go/29functionTypeDec.go
+++ basic-go/29functionTypeDec.go
@@ -1,4 +1,31 @@
-/* Function Type Declaration
+/*
+	Function Type Declaration
+
 - Kadang jika function terlalu panjang,  agak ribet untuk menuliskannya di dalam parameter
 - Type Declarations juga bisa digunakan membuat alias function, sehingga akan mempermudah kita menggunakan function sebagai parameter
 */
+package main
+
+import "fmt"
+
+type Filter func(string) string
+
+func sayHelloWithFilters(name string, filter Filter) {
+	fmt.Println("Hello", filter(name))
+}
+
+func spamFilters(name string) string {
+	if name == "Anjing" {
+		return "..."
+	}
+	if name == "Tai" {
+		return "..."
+	}
+	if name == "Asu" {
+		return "..."
+	}
+	return name
+}
+
+func main() {
+}
