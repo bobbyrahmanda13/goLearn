796f8e018d27280b6221c70ecef823255af20739
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-01-11 21:35:14 +0700

    update recover.go

--- basic-go/35recover.go
+++ basic-go/35recover.go
@@ -6,5 +6,29 @@
 
 package main
 
+import "fmt"
+
+func endAppp() {
+	fmt.Println("End App")
+
+	// # Recover yang benar
+
+	message := recover()
+	fmt.Println("Terjadi Error", message)
+}
+
+// # Recover yang salah
+
+func runAppp(error bool) {
+	defer endAppp()
+	if error {
+		panic("ERROR")
+	}
+
+	// message := recover()
+	// fmt.Println("Terjadi Error", message)
+}
+
 func main() {
+	runAppp(true)
 }
