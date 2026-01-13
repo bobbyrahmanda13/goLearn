50d69566787e7f174fec22d9c6ec246bb208b779
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-01-13 21:24:36 +0700

    update comment.go

---
+++ basic-go/36comment.go
@@ -0,0 +1,19 @@
+/*
+# Komentar
+- Komentar terbaik pada kode adalah kode itu sendiri
+- Saat membuat kode, kita perlu membuat kode semudah mungkin untuk dibaca
+- Namun Kadang juga kita butuh menambahkan komentar di kode kita
+*/
+package main
+
+/*
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+*/
+func main() {
+	// this is single line comment
+}
