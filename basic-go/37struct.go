3877eb582221bbd215b2fd2209e33ea2f05af427
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-01-18 02:04:14 +0700

    fix: indent

--- basic-go/37struct.go
+++ basic-go/37struct.go
@@ -57,11 +57,10 @@ func main() {
 	riko := Customer{"Budi", "Indonesia", 30}
 	fmt.Println(riko)
 
-	# Struct Method
-	- struct adalah tipe data seperti tipe data lainnya, dia bisa digunakan sebagai parameter untuk function
-	- Namun jika kita ingin menambahkan method ke dalam struct, sehingga sekan-akan sebuah struct memiliki function
-	- Method adalah function
-
-
-
+	/*
+		# Struct Method
+		- struct adalah tipe data seperti tipe data lainnya, dia bisa digunakan sebagai parameter untuk function
+		- Namun jika kita ingin menambahkan method ke dalam struct, sehingga sekan-akan sebuah struct memiliki function
+		- Method adalah function
+	*/
 }
