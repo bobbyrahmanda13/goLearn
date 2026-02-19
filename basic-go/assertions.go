9536035c386027d5ead53ba5eba771385cb1e348
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-02-20 05:35:28 +0700

    update assertions

---
+++ basic-go/assertions.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func random() any {
+	return true
+}
+
+func main() {
+	var result any = random()
+
+	// TEST:
+	// var resultString string = result.(string)
+	// fmt.Println(resultString)
+	// var resultInt int = result.(int)
+	// fmt.Println(resultInt)
+
+	// NOTE: Type Assertions Menggunakan Switch
+	// - saat salah menggunakan type assertions, maka bisa berakibat terjadi panic di aplikasi kita
+	// - jika panic dan tidak ter-recover, maka otomatis program kita akan mati
+	// - Agat lebih aman, sebaiknya kita menggunakan switch expression untuk melakukan type assertions
+
+	switch value := result.(type) {
+	case string:
+		fmt.Println("String", value)
+	case int:
+		fmt.Println("Int", value)
+	default:
+		fmt.Println("Unknown")
+	}
+}
