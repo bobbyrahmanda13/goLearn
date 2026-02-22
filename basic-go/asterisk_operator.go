1fc9c46034ea301a1671a166d37ac0f54243ee78
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-02-23 06:19:57 +0700

    update

---
+++ basic-go/asterisk_operator.go
@@ -0,0 +1,29 @@
+// NOTE:
+// * Operator *
+// - Saat kita mengubah variable pointer, maka yang barubah hanya variable tersebut,
+// - semua varible yang mengacu ke data yang sama tidak akan berubah
+// - jika kita ingin mengubah seluruh variable yang mengacu ke data tersbut, kita bisa menggunakan operator *
+
+package main
+
+import "fmt"
+
+type Addres struct {
+	City, Province, Country string
+}
+
+func main() {
+	var address1 Addres = Addres{"Padang", "Jagakarsa", "Jati"}
+	var address2 *Addres = &address1 // dengan pointer
+
+	// TEST:
+	// address1 := Address{"Padang", "Jagakarsa", "Jati"}
+	// address2 := &address1 // dengan pointer
+
+	address2.City = "Jakarta Selatan"
+
+	fmt.Println(address1) // ikut berubah
+	fmt.Println(address2) // berubah menjadi Jakarta Selatan
+
+	address2 = &Addres{"Bukittinggi", "Jakarta Selatan", "Indonesia"}
+}
