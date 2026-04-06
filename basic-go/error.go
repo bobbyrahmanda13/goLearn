8b03bc8dcb6705ea411bd2bc261c42e881e495ce
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-04-06 20:13:30 +0700

    jadikan error ke variable

--- basic-go/error.go
+++ basic-go/error.go
@@ -5,16 +5,18 @@ import (
 	"fmt"
 )
 
+var errorBro = errors.New("Pembagian dengan 0")
+
 func Pembagian(nilai int, pembagian int) (int, error) {
 	if pembagian == 0 {
-		return 0, errors.New("Pembagian dengan 0")
+		return 0, errorBro
 	} else {
 		return nilai / pembagian, nil
 	}
 }
 
 func main() {
-	hasil, err := Pembagian(20, 5)
+	hasil, err := Pembagian(20, 0)
 	if err == nil {
 		fmt.Println("Hasil", hasil)
 	} else {
